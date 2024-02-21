f7413cd60a3401ef04f4667a2ed183ad0f07bf5b
root <diego.b.c.rodriguez@gmail.com>
2024-02-21 22:28:37 +0000

    Edited the file 4- to betty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
-	#include <stdio.h>
+#include <stdio.h>
 
-	/**
-	 * main - Puts "Programming is like building a multilingual puzzle"
-	 *
-	 * Return - Always 0 (succes)
-	 */
-	int main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
-	}
+/**
+ * main - Puts "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
