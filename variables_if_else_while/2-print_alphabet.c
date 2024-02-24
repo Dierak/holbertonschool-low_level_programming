af44f589722fc32278a166aaf2820a66c1205566
root <diego.b.c.rodriguez@gmail.com>
2024-02-24 00:18:09 +0000

    Correction on file 2-

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{	
+		putchar(c);
+	}
+	putchar('\n');
 
 	return (0);
 }
