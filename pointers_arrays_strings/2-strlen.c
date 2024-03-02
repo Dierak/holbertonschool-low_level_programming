f882fb96204a5d8b30a2b24eb96c0c60fdbb17a4
root <diego.b.c.rodriguez@gmail.com>
2024-03-02 20:03:46 +0000

    Edited file 2-

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -5,14 +5,12 @@
  * @s: string
  * Return: returns length;
  */
-
-int _strlen(char *s )
+int _strlen(char *s)
 {
-	int count, inc;
-	inc = 0;
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
 
-	for (count = 0; s[count] != '\0'; count++)
-	inc++;
-
-	return (inc);
+return (inc);
 }
