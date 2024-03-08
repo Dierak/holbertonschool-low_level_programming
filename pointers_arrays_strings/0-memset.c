5ab216486c951ee0fa06a91924d0dec5391bf0a3
root <diego.b.c.rodriguez@gmail.com>
2024-03-08 00:32:46 +0000

    Edited documentation

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
+ * _memset - fills memory with a constant byte
+ * @s: first value
+ * @b: second value
+ * @n: thirth value
+ * Return: new value of target
  */
 
 char *_memset(char *s, char b, unsigned int n)
