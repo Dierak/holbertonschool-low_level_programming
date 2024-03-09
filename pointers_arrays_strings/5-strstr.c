6590f3dbd94e495b461209e80b0870bfcd735116
root <diego.b.c.rodriguez@gmail.com>
2024-03-09 15:51:37 +0000

    Edited documentation

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  * _strstr - finds the first occurrence of the substring needle in the string
  * haystack. The terminating null bytes (\0) are not compared.
  * @haystack: string where the search is made
- * @neddle: string whose occurence is searched in haystack
+ * @needle: string whose occurence is searched in haystack
  * Return: a pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
  */
