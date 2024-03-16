df07c0946fae7ced8a757072cbfa75114168f823
root <diego.b.c.rodriguez@gmail.com>
2024-03-16 17:41:16 +0000

    Added header file

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
+
+#include <stdlib.h>
+
 /**
  * prototypes working with malloc, free
  */
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
